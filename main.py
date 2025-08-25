2842fe3431643eab97bb060431110b7de73e4612
Travline <poluxmorales1@gmail.com>
2025-08-24 22:33:19 -0500

    first test

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+async def connectiondb():
+    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+    return conn.cursor()
 
 app = FastAPI()
 
 @app.get("/")
 async def test():
-    return [{
-                "ola":"oal"
-            },
-            {
-                "oli":"increible"
-            }]
+    cur = await connectiondb()
+    cur.execute("Select * FROM users;")
+    response = (cur.fetchone())[1]
+    return {"email":str(response)}
