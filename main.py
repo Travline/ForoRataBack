66f45929ee07e5ac4cac0f0764770e916e6d9ab1
Travline <poluxmorales1@gmail.com>
2025-11-20 11:45:09 -0500

    debugginng me endpoint

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ app.include_router(posts_router, prefix="/posts")
 async def me(user_id:Optional[str] = Depends(get_current_user)):
     try:
         if user_id is None:
-            return HTTPException(status_code=401, detail="No loged")
+            return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={'message':'No loged'})
         return {"me" : user_id}
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error: {str(e)}")
