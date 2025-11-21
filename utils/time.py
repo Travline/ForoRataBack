36aa6c19391b4084dff1cca53cd713b320c0d90d
Travline <poluxmorales1@gmail.com>
2025-11-21 15:17:24 -0500

    time calcs finished

--- utils/time.py
+++ utils/time.py
@@ -4,4 +4,15 @@ async def delta_between_dates(start_date: datetime) -> str:
   end_date = int(datetime.now(UTC).timestamp())
   start_date = int(start_date.timestamp())
   delta = end_date - start_date
-  return f"{delta} secs"
+  if delta > 31536000:
+    return f"{delta/31536000} y"
+  if delta > 2592000:
+    return f"{delta/2592000} m"
+  if delta > 86400:
+    return f"{delta/86400} d"
+  if delta > 3600:
+    return f"{delta/3600} h"
+  if delta > 60:
+    return f"{delta/60} min"
+  if delta > 0:
+    return f"{delta/0} s  "
