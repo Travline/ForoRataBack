f2142a3e415045636edbb75c74fcc68879294266
Travline <poluxmorales1@gmail.com>
2025-11-21 18:39:26 -0500

    start server

--- main.py
+++ main.py
@@ -50,3 +50,4 @@ async def health():
         return {"message": "increible"}
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error: {str(e)}")
+    
